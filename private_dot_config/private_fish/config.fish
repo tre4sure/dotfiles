6e388d983d549424384d99ff92f41e3cb9a8cc70
treasure <caochen_workmail@foxmail.com>
2023-07-04 15:52:58 +0800

    remove fish_variables and add some variables

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,5 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -x EDITOR nvim
-set -x PATH /usr/local/go/bin $PATH
+set -gx EDITOR nvim
+set -gx VISUAL $EDITOR
+
+set -Ux fish_user_paths # set -U make effective throughout the entire system
+
+fish_add_path ~/scripts/
+
+
+fish_add_path /usr/local/go/bin
+fish_add_path ~/go/bin
+
+
+abbr vim nvim
+abbr vi nvim
+abbr v nvim
+abbr ec "chezmoi edit --apply"
